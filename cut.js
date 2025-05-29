8e5f5bef825d1aac0f9d92821fc3a5e36fb1ec92
ImLosss <pahriansyahryan@gmail.com>
2025-05-29 23:25:27 +0800

    update

---
+++ cut.js
@@ -0,0 +1,115 @@
+const ffmpegStatic = require('ffmpeg-static');
+const { spawn } = require('child_process');
+
+async function cutFast(inputFile, outputFile) {
+    return new Promise((resolve, reject) => {
+      const ffmpegArgs = [
+        '-y',
+        '-i', inputFile,
+        '-ss', '00:02:55', 
+        '-to', '00:20:57',
+        '-c', 'copy',
+        outputFile
+      ];
+  
+      // Jalankan ffmpeg dengan argumen yang telah ditentukan
+      const ffmpegProcess = spawn(ffmpegStatic, ffmpegArgs);
+  
+      ffmpegProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+      });
+  
+      ffmpegProcess.stderr.on('data', (data) => {
+        console.log(`Process: ${data}`);
+      });
+
+      ffmpegProcess.stderr.on('error', (err) => {
+        console.log(err)
+      });
+  
+      ffmpegProcess.on('close', (code) => {
+        if (code === 0) {
+          resolve('Konversi MP4A ke MP3 selesai.');
+        } else {
+          reject(new Error(`Proses ffmpeg berakhir dengan kode error ${code}`));
+        }
+      });
+    });
+}
+
+async function cut(inputFile, outputFile) {
+  return new Promise((resolve, reject) => {
+    const ffmpegArgs = [
+      '-y', // Overwrite file output jika ada
+      '-i', inputFile, // File input
+      '-ss', '00:02:29', 
+      '-to', '00:18:09',
+      '-vf', 'scale=1920:-1', // Atur lebar menjadi 1920, tinggi mengikuti rasio aspek
+      // '-r', '25', // Atur FPS menjadi 25
+      '-c:v', 'h264_amf', // Gunakan GPU AMD dengan encoder h264_amf
+      '-cq:v', '18', // Gunakan CRF rendah untuk kualitas tinggi (angka lebih rendah = kualitas lebih baik)
+      '-rc', 'cbr', // Gunakan Variable Bitrate untuk membatasi bitrate maksimum
+      '-b:v', '5M', // Bitrate target rata-rata
+      '-maxrate', '5M', // Batasi bitrate maksimum hingga 5 Mbps
+      '-bufsize', '10M', // Buffer size dua kali maksimum bitrate
+      '-quality', 'quality',
+      '-preset', 'quality', // Gunakan preset kualitas GPU
+      '-c:a', 'aac',
+      outputFile // File output
+    ];
+
+    // const ffmpegArgs = [
+    //   '-y', // Overwrite file output jika ada
+    //   '-i', inputFile, // File input
+    //   '-vf', "scale=1280:-1",
+    //   '-c:v', 'libx264',
+    //   '-crf', '30',
+    //   // '-b:v', '2M', // Bitrate target rata-rata
+    //   '-maxrate', '5M', // Batasi bitrate maksimum hingga 5 Mbps
+    //   '-bufsize', '10M', // Buffer size dua kali maksimum bitrate
+    //   '-preset', 'fast',
+    //   '-c:a', 'copy', 
+    //   outputFile // File output
+    // ];
+
+  
+    // Jalankan ffmpeg dengan argumen yang telah ditentukan
+    const ffmpegProcess = spawn(ffmpegStatic, ffmpegArgs);
+
+    ffmpegProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ffmpegProcess.stderr.on('data', (data) => {
+      console.log(`Process: ${data}`);
+    });
+
+    ffmpegProcess.stderr.on('error', (err) => {
+      console.log(err)
+    });
+
+    ffmpegProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve('Konversi MP4A ke MP3 selesai.');
+      } else {
+        reject(new Error(`Proses ffmpeg berakhir dengan kode error ${code}`));
+      }
+    });
+  });
+}
+//  const ffmpegArgs = [
+//       '-y',
+//       '-i', inputFile,
+//       // '-ss', '00:03:09',             // Tempatkan -ss sebelum input untuk pemotongan cepat
+//       // '-to', '00:18:10',             // Tentukan waktu akhir
+//       '-c:v', 'libx264',             // Menggunakan encoder h.264
+//       '-crf', '22',                  // Atur CRF untuk menjaga kualitas (20-23 ideal untuk kualitas tinggi)
+//       '-preset', 'slow',             // 'slow' atau 'veryslow' untuk kompresi yang lebih baik
+//       '-c:a', 'aac',                 // Re-encode audio
+//       '-b:a', '128k',                // Turunkan bitrate audio ke 128k untuk ukuran lebih kecil
+//       '-maxrate', '3M',              // Batasi bitrate video maksimum
+//       '-bufsize', '4M',              // Buffer size untuk kontrol bitrate
+//       outputFile
+//     ];
+
+cut('217_4K.mp4', 'PW_EP217.mp4');
---
+++ sub.js
@@ -0,0 +1,209 @@
+const ffmpegStatic = require('ffmpeg-static');
+const { spawn } = require('child_process');
+
+async function burnSubtitle(inputFile, outputFile) {
+  return new Promise((resolve, reject) => {
+    const ffmpegArgs = [
+      '-y',
+      '-i', inputFile,
+      '-c:v', 'libx264',
+      '-crf', '35',
+      '-preset', 'slow',
+      '-vf', '-vf', `subtitles=btth/karaoke_ext.ass,
+        drawtext=text='DongWorld':font=Verdana:fontsize=30:fontcolor=white@0.5:x=15:y=15`,
+      '-c:a', 'copy',  
+      '-maxrate', '2M', 
+      '-bufsize', '4M', 
+      outputFile
+    ];
+
+    // btth : 25
+    // renegade : 40
+
+    // Jalankan ffmpeg dengan argumen yang telah ditentukan
+    const ffmpegProcess = spawn(ffmpegStatic, ffmpegArgs);
+
+    ffmpegProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ffmpegProcess.stderr.on('data', (data) => {
+      console.log(`Process: ${data}`);
+    });
+
+    ffmpegProcess.stderr.on('error', (err) => {
+      console.log(err)
+    });
+
+    ffmpegProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve('Konversi MP4A ke MP3 selesai.');
+      } else {
+        reject(new Error(`Proses ffmpeg berakhir dengan kode error ${code}`));
+      }
+    });
+  });
+}
+
+async function burnSubtitleGPU(inputFile, outputFile) {
+  return new Promise((resolve, reject) => {
+    const ffmpegArgs = [
+      '-y', // Overwrite file output jika ada
+      '-i', inputFile, // File input
+      '-c:v', 'h264_amf', // Gunakan GPU AMD dengan encoder h264_amf
+      '-cq:v', '18', // Gunakan CRF rendah untuk kualitas tinggi (angka lebih rendah = kualitas lebih baik)
+      '-rc', 'cbr', // Gunakan Variable Bitrate untuk membatasi bitrate maksimum
+      '-b:v', '3.5M', // Bitrate target rata-rata
+      '-maxrate', '3.5M', // Batasi bitrate maksimum hingga 5 Mbps
+      '-bufsize', '6M', // Buffer size dua kali maksimum bitrate
+      '-quality', 'quality', // Prioritaskan kualitas
+      '-preset', 'quality', // Gunakan preset kualitas GPU
+      // '-vf', `subtitles=output.srt:force_style='FontName=ArialMT,Bold=1,FontSize=16,PrimaryColour=&HFFFFFF&,Outline=0.5,MarginV=25',
+      // scale=1920:-1
+      '-vf', "subtitles=PW_EP217_INDO.ass,drawtext=text='DongWorld':font=Verdana:fontsize=30:fontcolor=white@0.5:x=15:y=15",
+      '-c:a', 'copy', 
+      outputFile // File output
+    ];
+    // btth : 25
+    // renegade : 40
+    //pw : 33
+
+    // const ffmpegArgs = [
+    //   '-y', // Overwrite file output jika ada
+    //   '-i', inputFile, // File input
+    //   //scale=-1:1080
+    //   '-vf', "subtitles=pv.ass,drawtext=text='DongWorld':font=Verdana:fontsize=20:fontcolor=white@0.5:x=15:y=15",
+    //   '-c:v', 'libx264',
+    //   '-crf', '25',
+    //   // '-b:v', '2M', // Bitrate target rata-rata
+    //   '-maxrate', '5M', // Batasi bitrate maksimum hingga 5 Mbps
+    //   '-bufsize', '10M', // Buffer size dua kali maksimum bitrate
+    //   '-preset', 'fast',
+    //   '-c:a', 'copy', 
+    //   outputFile // File output
+    // ];
+
+    // Jalankan ffmpeg dengan argumen yang telah ditentukan
+    const ffmpegProcess = spawn(ffmpegStatic, ffmpegArgs);
+
+    ffmpegProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ffmpegProcess.stderr.on('data', (data) => {
+      console.log(`Process: ${data}`);
+    });
+
+    ffmpegProcess.stderr.on('error', (err) => {
+      console.log(err)
+    });
+
+    ffmpegProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve('Konversi MP4A ke MP3 selesai.');
+      } else {
+        reject(new Error(`Proses ffmpeg berakhir dengan kode error ${code}`));
+      }
+    });
+  });
+}
+
+
+async function embedSubtitle(inputFile, outputFile) {
+    return new Promise((resolve, reject) => {
+      const ffmpegArgs = [
+        '-y',
+        '-i', inputFile,          // Input video (yang sudah ada subtitle Chinese)
+        '-i', 'BTTH_EP146_INDO.ass',   // Subtitle baru (format SRT)
+        '-map', '0:v',            // Ambil video dari input pertama
+        '-map', '0:a',            // Ambil audio dari input pertama
+        '-map', '1:s',            // Ambil subtitle dari input kedua (file SRT)
+        '-c:v', 'copy',           // Copy video tanpa re-encode
+        '-c:a', 'copy',           // Copy audio tanpa re-encode
+        // '-c:s', 'mov_text',       // Format subtitle untuk MP4 (jika output MP4)
+        '-c:s', 'ass',          // Jika output MKV/WebM dan ingin format ASS
+        '-disposition:s:0', 'default',  // Set subtitle baru sebagai default
+        outputFile
+    ];
+  
+      // Jalankan ffmpeg dengan argumen yang telah ditentukan
+      const ffmpegProcess = spawn(ffmpegStatic, ffmpegArgs);
+  
+      ffmpegProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+      });
+  
+      ffmpegProcess.stderr.on('data', (data) => {
+        console.log(`Process: ${data}`);
+      });
+
+      ffmpegProcess.stderr.on('error', (err) => {
+        console.log(err)
+      });
+  
+      ffmpegProcess.on('close', (code) => {
+        if (code === 0) {
+          resolve('Konversi MP4A ke MP3 selesai.');
+        } else {
+          reject(new Error(`Proses ffmpeg berakhir dengan kode error ${code}`));
+        }
+      });
+    });
+}
+
+async function extractSrt(inputFile) {
+    return new Promise((resolve, reject) => {
+        const ffmpegArgs = [
+            '-y',
+            '-i', inputFile,
+            '-map', '0:s:0',  // Pilih stream subtitle dari input
+            'output.srt'      // Nama file output untuk subtitle
+        ];
+    
+        // Jalankan ffmpeg dengan argumen yang telah ditentukan
+        const ffmpegProcess = spawn(ffmpegStatic, ffmpegArgs);
+    
+        ffmpegProcess.stdout.on('data', (data) => {
+          console.log(`stdout: ${data}`);
+        });
+    
+        ffmpegProcess.stderr.on('data', (data) => {
+          console.log(`Process: ${data}`);
+        });
+  
+        ffmpegProcess.stderr.on('error', (err) => {
+          console.log(err)
+        });
+    
+        ffmpegProcess.on('close', (code) => {
+          if (code === 0) {
+            resolve('Konversi MP4A ke MP3 selesai.');
+          } else {
+            reject(new Error(`Proses ffmpeg berakhir dengan kode error ${code}`));
+          }
+        });
+    });
+}
+
+
+// // with logo
+// const ffmpegArgs = [
+//   '-y', // Overwrite file output jika ada
+//   '-i', inputFile, // File input
+//   '-i', 'logo.png', // File logo PNG
+//   '-ss', '00:00:20', // Tempatkan -ss sebelum input untuk pemotongan cepat
+//   '-to', '00:00:30',
+//   '-filter_complex', '[1:v]scale=100:-1[logo];[0:v][logo]overlay=10:10', // Atur ukuran logo menjadi 100x100 dan tempatkan di posisi (10,10)
+//   '-c:v', 'h264_amf', // Gunakan GPU AMD dengan encoder h264_amf
+//   '-cq:v', '18', // Gunakan CRF rendah untuk kualitas tinggi
+//   '-rc', 'cbr', // Gunakan Variable Bitrate untuk membatasi bitrate maksimum
+//   '-b:v', '3M', // Bitrate target rata-rata
+//   '-maxrate', '3M', // Batasi bitrate maksimum
+//   '-bufsize', '6M', // Buffer size dua kali maksimum bitrate
+//   '-quality', 'quality', // Prioritaskan kualitas
+//   '-preset', 'quality', // Gunakan preset kualitas GPU
+//   '-c:a', 'copy', // Salin audio tanpa perubahan
+//   outputFile // File output
+// ];
+
+burnSubtitleGPU('PW_EP217.mp4', 'PW_EP217_INDO.mp4')
