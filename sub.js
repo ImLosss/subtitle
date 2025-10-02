e455aa085e7d50e03c4e0d4626a79a1bca476666
ImLosss <pahriansyahryan@gmail.com>
2025-10-02 21:06:03 +0800

    update

--- sub.js
+++ sub.js
@@ -76,7 +76,7 @@ async function burnSubtitleGPU(inputFile, outputFile) {
       // '-ss', '01:18:40',
       // '-to', '01:19:25',
       // scale=-1:1080
-      '-vf', "subtitles=JD_EP22_INDO.ass,drawtext=text='DongWorld':font=Verdana:fontsize=25:fontcolor=white@0.5:x=15:y=15",
+      '-vf', "subtitles=JD_EP23_INDO.ass,drawtext=text='DongWorld':font=Verdana:fontsize=25:fontcolor=white@0.5:x=15:y=15",
       '-c:v', 'libx264',
       '-crf', '25',
       // '-b:v', '2M', // Bitrate target rata-rata
@@ -210,4 +210,4 @@ async function extractSrt(inputFile) {
 //   outputFile // File output
 // ];
 
-burnSubtitleGPU('JD_EP22.mp4', 'JD_EP22_INDO.mp4')
+burnSubtitleGPU('JD_EP23.mp4', 'JD_EP23_INDO.mp4')
